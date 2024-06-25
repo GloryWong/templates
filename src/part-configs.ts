347e12eafee286a13f00287b102616ab9596fa7a
shit <glorywong1001@gmail.com>
2024-06-25 17:31:39 +0800

    feat(parts): update parts

--- src/part-configs.ts
+++ src/part-configs.ts
@@ -2,14 +2,41 @@ import { getGitConfigs } from './getGitConfig.js'
 import { definePartConfigs } from './definePartConfigs.js'
 
 export const partConfigs = definePartConfigs({
-  commitlint: {
+  commitlintstaged: {
     destDir: '.',
+    packageJsonUpdates: {
+      'scripts': {
+        'prepare': 'pnpm simple-git-hooks',
+        'lint': 'eslint .',
+        'lint:fix': 'pnpm lint --fix',
+      },
+      'devDependencies': {
+        '@commitlint/cli': '^19',
+        '@commitlint/config-conventional': '^19',
+        'lint-staged': '^15',
+        'simple-git-hooks': '^2',
+      },
+      'simple-git-hooks': {
+        'pre-commit': 'pnpm lint-staged',
+        // eslint-disable-next-line no-template-curly-in-string
+        'commit-msg': 'pnpm commitlint --edit ${1}',
+      },
+      'lint-staged': {
+        '*': 'pnpm lint:fix',
+      },
+    },
   },
   eslint: {
     destDir: '.',
     packageJsonUpdates: {
+      scripts: {
+        'lint': 'eslint .',
+        'lint:fix': 'pnpm lint --fix',
+      },
       devDependencies: {
-        eslint: '^8',
+        '@antfu/eslint-config': '^2',
+        'eslint': '^8',
+        'eslint-plugin-perfectionist': '^2',
       },
     },
   },
@@ -22,7 +49,12 @@ export const partConfigs = definePartConfigs({
   typescript: {
     destDir: '.',
     packageJsonUpdates: {
+      engines: {
+        node: '>=20',
+      },
       devDependencies: {
+        'typescript': '^5',
+        '@tsconfig/node20': '^20',
         'type-fest': 'latest',
       },
     },
