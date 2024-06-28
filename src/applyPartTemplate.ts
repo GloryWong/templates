b1dbbeb64e54c60fe06531a668dcd4e09074ddd2
GloryWong <glorywong1001@gmail.com>
2024-06-28 22:41:31 +0800

    feat: add cli spinner

--- src/applyPartTemplate.ts
+++ src/applyPartTemplate.ts
@@ -2,6 +2,8 @@ import { join } from 'node:path'
 import { readdir } from 'node:fs/promises'
 import { downloadTemplate } from 'giget'
 import { updatePackage } from 'write-package'
+import ora from 'ora'
+import { isLoggerEnabled } from '@gloxy/logger'
 import { getTmpPath } from './utils/getTmpPath.js'
 import { deleteTmp } from './utils/deleteTmp.js'
 import type { CopyTemplateOptions } from './copyTemplate.js'
@@ -21,29 +23,36 @@ export interface ApplyPartTemplateOptions extends CopyTemplateOptions {
 
 export async function applyPartTemplate(partId: string, options: ApplyPartTemplateOptions = {}) {
   try {
-    const log = logger('applyPartTemplate')
     const config = configs.get(partId)
-
     if (!config)
       throw new Error(`Invalid partId`)
 
     const { force, merge, variables, install = false } = options
+    const log = logger('applyPartTemplate')
+    const spinner = ora({
+      isSilent: isLoggerEnabled('templates:*'),
+    })
 
     config.skipTemplate && log.debug('Skip template download and copy. %s', partId)
     if (!config.skipTemplate) {
       // Download parts to tmp
       log.debug('Downloading template from %s', config.src)
+      spinner.start('Downloading template...')
       const tmp = await getTmpPath(TEMPLATE_DOWNLOAD_DIR)
       const { source, dir } = await downloadTemplate(config.src, {
         dir: join(tmp, partId),
       })
-      if (!(await readdir(dir)).length)
+      if (!(await readdir(dir)).length) {
+        spinner.fail('Failed to download template')
         throw new Error(`Failed to download template from ${source}`)
+      }
 
+      spinner.succeed('Downloaded template')
       log.debug('Downloaded template to %s', dir)
 
       // Copy tmp to destination
       log.debug('Copying template to %s', config.destDir)
+      spinner.start('Copying template...')
       await copyTemplate(dir, config.destDir, {
         force,
         merge,
@@ -52,6 +61,7 @@ export async function applyPartTemplate(partId: string, options: ApplyPartTempla
           ...variables,
         },
       })
+      spinner.succeed('Copied template')
       log.debug('Copied template. %s', partId)
     }
 
@@ -59,7 +69,9 @@ export async function applyPartTemplate(partId: string, options: ApplyPartTempla
     const { packageJsonUpdates } = config
     if (packageJsonUpdates) {
       log.debug('Updating package json. %s', partId)
+      spinner.start('Updating package.json...')
       await updatePackage(packageJsonUpdates)
+      spinner.succeed('Updated package.json')
       log.debug('Updated package json. %s', partId)
     }
 
@@ -70,7 +82,9 @@ export async function applyPartTemplate(partId: string, options: ApplyPartTempla
     // Install dependencies
     if (install) {
       log.debug('Installing dependencies. %s', partId)
+      spinner.start('Installing dependencies...')
       await installDeps(config)
+      spinner.succeed('Installed dependencies')
       log.debug('Installed dependencies. %s', partId)
     }
   }
