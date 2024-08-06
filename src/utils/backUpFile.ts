7898161773ea4df4283183e5aaf7c3fd555d1d68
GloryWong <glorywong1001@gmail.com>
2024-08-07 03:23:42 +0800

    chore: upgrade deps

--- src/utils/backUpFile.ts
+++ src/utils/backUpFile.ts
@@ -9,7 +9,7 @@ export async function backUpFile(path: string, destDirPath?: string) {
   const rawFileName = basename(path, extName)
   const backupDir = destDirPath ?? await getTmpPath('backups')
   await ensureDir(backupDir)
-  const dest = join(backupDir, `${rawFileName}.backup-${timestamp(undefined, { excludeMillisecond: true })}${extName}`)
+  const dest = join(backupDir, `${rawFileName}.backup-${timestamp({ excludeMillisecond: true })}${extName}`)
 
   await copy(path, dest)
   logger.warn('Original %s was backed up to %s', basename(path), dest)
